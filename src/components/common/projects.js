a5ab4edc003355e4ec7a256ad71470030563fbea
Confetto22 <enockagyei1999@gmail.com>
2025-06-07 16:26:59 +0100

    minor-tweak

--- src/components/common/projects.js
+++ src/components/common/projects.js
@@ -1,23 +1,21 @@
 export const projects = [
   {
-    name: "Enforcement Mass media campaign",
-    year: "2023",
-    genre: "commercial",
+    name: "School Girl(PSA)",
+    year: "2019",
+    genre: "media campaign",
     slideshow: [
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291900/pyrite/ENFORCEMENT-PSA/QLIM9350-Edit_uavajh.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291899/pyrite/ENFORCEMENT-PSA/QLIM9371-Edit_doabcm.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291899/pyrite/ENFORCEMENT-PSA/QLIM9338-Edit_fxycuj.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291901/pyrite/ENFORCEMENT-PSA/QLIM9458_i4pdkh.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291898/pyrite/ENFORCEMENT-PSA/QLIM9419_kl9ije.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4383_bmufz3.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4392_t7yuqn.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4805_fxo4zn.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4424_gywbhc.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4896_eoq3yu.webp",
     ],
     desc: {
       pargagraph1:
-        "With cutting edge technology, JILOVA adapted an Ethiopian Public Service Announcement (PSA) for our clients to use in our Ghanaian context. Creativity, precision and dynamism were exhibited at its peak to re-create an extremely effective PSA. ",
-      pargagraph2:
-        "The Enforcement Mass Media Campaign was a strategic initiative aimed at reducing road accidents caused by speeding. Jilova Studios developed a compelling narrative that highlighted the dangers of speeding and the importance of adhering to speed limits. Through a combination of impactful visuals, engaging storytelling, and targeted messaging, the campaign sought to influence driver behavior and foster a culture of safety on the roads.",
+        "The School Girl PSA tells the heart-wrenching story of a young girl who loses her life on her way to school due to a road crash. With precision and creativity at its core, this PSA was crafted to evoke strong emotions and grab the attention of viewers. The production quality was so high that many viewers believed the story to be real, underscoring the campaign's effectiveness in conveying its message.",
+      pargagraph2: "",
     },
     refLink: "#",
-
     cardDetails: [
       {
         name: "Client",
@@ -25,14 +23,14 @@ export const projects = [
         value: "National Road Safety Authority",
       },
       // {
-      //   name: "Project  Name",
+      //   name: "Project Name",
       //   icon: "bx bx-file-find",
-      //   value: "Butterfly",
+      //   value: "School Girl(PSA)",
       // },
       {
         name: "Project Year",
         icon: "bx bx-calendar",
-        value: "2023",
+        value: "2019",
       },
       {
         name: "Location",
@@ -42,12 +40,12 @@ export const projects = [
       {
         name: "Category",
         icon: "bx bx-folder-open",
-        value: "commercial",
+        value: "Media Campaign",
       },
       {
         name: "duration",
         icon: "bx bx-folder-open",
-        value: "1 day",
+        value: "1 Day",
       },
     ],
     projectList: [
@@ -58,21 +56,21 @@ export const projects = [
       "Event Photography",
       "360 Videography",
     ],
-
-    img: "bg-[url('https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291898/pyrite/ENFORCEMENT-PSA/QLIM9419_kl9ije.webp')]",
+    img: "bg-[url('https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/school-girl-psa/401A4896_eoq3yu.webp')]",
     overview:
-      "The Enforcement Mass Media Campaign successfully raised awareness about the dangers of speeding and contributed to a shift in driver behavior. By leveraging compelling storytelling and strategic media placement, the campaign reached a wide audience and reinforced the importance of road safety. Jilova Studios played a pivotal role in conceptualizing, producing, and executing the campaign. Our team of experts worked closely with the client to understand their objectives and deliver a campaign that met and exceeded expectations. From creative development to media outreach, we ensured that every aspect of the campaign was meticulously planned and executed.",
+      "The objective of this project was to create a powerful and impactful public service announcement (PSA) that highlights the tragic consequences of road crashes, particularly for vulnerable road users such as school children. The School Girl PSA generated significant attention and sympathy from viewers, sparking conversations about road safety and the need for greater awareness and action. The campaign's impact was amplified by its ability to evoke strong emotions and create a lasting impression on its audience. Jilova Studios played a pivotal role in conceptualizing, producing, and executing the School Girl PSA. Our team of experts worked tirelessly to craft a compelling narrative that would resonate with audiences and drive home the importance of road safety",
     execution: {
       writeUp:
-        "The execution of the Enforcement Mass Media Campaign involved careful planning and attention to detail. Key aspects included:",
+        "The execution of the School Girl PSA involved meticulous planning and attention to detail to ensure a high-quality final product. Key aspects of the project execution included:",
       points: [
-        "Concept Development: Developed a strong creative concept that would resonate with the target audience.",
-        "Content Creation: Produced high-quality multimedia content that would grab attention and convey the message effectively.",
-        "Media Planning: Implemented a comprehensive media outreach strategy to maximize reach and engagement.",
-        "Message Delivery: Crafted messages that would encourage drivers to adopt safer driving habits.",
+        "Script Development: Crafted a compelling script that told a heart-wrenching story while conveying a strong message about road safety.",
+        "Talent Selection: Selected talented actors who could bring the story to life and evoke the desired emotional response from viewers.",
+        "Location Scouting: Identified suitable locations that would add to the realism and impact of the PSA.",
+        "Post-Production: Utilized advanced editing and visual effects techniques to enhance the narrative and create a polished final product.",
       ],
     },
   },
+
   {
     name: "Road safety week",
     year: "2021",
@@ -218,22 +216,24 @@ export const projects = [
     },
   },
   {
-    name: "School Girl(PSA)",
-    year: "2019",
-    genre: "media campaign",
+    name: "Enforcement Mass media campaign",
+    year: "2023",
+    genre: "commercial",
     slideshow: [
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4383_bmufz3.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4392_t7yuqn.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4805_fxo4zn.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4424_gywbhc.webp",
-      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/401A4896_eoq3yu.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291900/pyrite/ENFORCEMENT-PSA/QLIM9350-Edit_uavajh.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291899/pyrite/ENFORCEMENT-PSA/QLIM9371-Edit_doabcm.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291899/pyrite/ENFORCEMENT-PSA/QLIM9338-Edit_fxycuj.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291901/pyrite/ENFORCEMENT-PSA/QLIM9458_i4pdkh.webp",
+      "https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291898/pyrite/ENFORCEMENT-PSA/QLIM9419_kl9ije.webp",
     ],
     desc: {
       pargagraph1:
-        "The School Girl PSA tells the heart-wrenching story of a young girl who loses her life on her way to school due to a road crash. With precision and creativity at its core, this PSA was crafted to evoke strong emotions and grab the attention of viewers. The production quality was so high that many viewers believed the story to be real, underscoring the campaign's effectiveness in conveying its message.",
-      pargagraph2: "",
+        "With cutting edge technology, JILOVA adapted an Ethiopian Public Service Announcement (PSA) for our clients to use in our Ghanaian context. Creativity, precision and dynamism were exhibited at its peak to re-create an extremely effective PSA. ",
+      pargagraph2:
+        "The Enforcement Mass Media Campaign was a strategic initiative aimed at reducing road accidents caused by speeding. Jilova Studios developed a compelling narrative that highlighted the dangers of speeding and the importance of adhering to speed limits. Through a combination of impactful visuals, engaging storytelling, and targeted messaging, the campaign sought to influence driver behavior and foster a culture of safety on the roads.",
     },
     refLink: "#",
+
     cardDetails: [
       {
         name: "Client",
@@ -241,14 +241,14 @@ export const projects = [
         value: "National Road Safety Authority",
       },
       // {
-      //   name: "Project Name",
+      //   name: "Project  Name",
       //   icon: "bx bx-file-find",
-      //   value: "School Girl(PSA)",
+      //   value: "Butterfly",
       // },
       {
         name: "Project Year",
         icon: "bx bx-calendar",
-        value: "2019",
+        value: "2023",
       },
       {
         name: "Location",
@@ -258,12 +258,12 @@ export const projects = [
       {
         name: "Category",
         icon: "bx bx-folder-open",
-        value: "Media Campaign",
+        value: "commercial",
       },
       {
         name: "duration",
         icon: "bx bx-folder-open",
-        value: "1 Day",
+        value: "1 day",
       },
     ],
     projectList: [
@@ -274,17 +274,18 @@ export const projects = [
       "Event Photography",
       "360 Videography",
     ],
-    img: "bg-[url('https://res.cloudinary.com/dv9aqxptd/image/upload/v1749290030/pyrite/school-girl-psa/401A4896_eoq3yu.webp')]",
+
+    img: "bg-[url('https://res.cloudinary.com/dv9aqxptd/image/upload/v1749291898/pyrite/ENFORCEMENT-PSA/QLIM9419_kl9ije.webp')]",
     overview:
-      "The objective of this project was to create a powerful and impactful public service announcement (PSA) that highlights the tragic consequences of road crashes, particularly for vulnerable road users such as school children. The School Girl PSA generated significant attention and sympathy from viewers, sparking conversations about road safety and the need for greater awareness and action. The campaign's impact was amplified by its ability to evoke strong emotions and create a lasting impression on its audience. Jilova Studios played a pivotal role in conceptualizing, producing, and executing the School Girl PSA. Our team of experts worked tirelessly to craft a compelling narrative that would resonate with audiences and drive home the importance of road safety",
+      "The Enforcement Mass Media Campaign successfully raised awareness about the dangers of speeding and contributed to a shift in driver behavior. By leveraging compelling storytelling and strategic media placement, the campaign reached a wide audience and reinforced the importance of road safety. Jilova Studios played a pivotal role in conceptualizing, producing, and executing the campaign. Our team of experts worked closely with the client to understand their objectives and deliver a campaign that met and exceeded expectations. From creative development to media outreach, we ensured that every aspect of the campaign was meticulously planned and executed.",
     execution: {
       writeUp:
-        "The execution of the School Girl PSA involved meticulous planning and attention to detail to ensure a high-quality final product. Key aspects of the project execution included:",
+        "The execution of the Enforcement Mass Media Campaign involved careful planning and attention to detail. Key aspects included:",
       points: [
-        "Script Development: Crafted a compelling script that told a heart-wrenching story while conveying a strong message about road safety.",
-        "Talent Selection: Selected talented actors who could bring the story to life and evoke the desired emotional response from viewers.",
-        "Location Scouting: Identified suitable locations that would add to the realism and impact of the PSA.",
-        "Post-Production: Utilized advanced editing and visual effects techniques to enhance the narrative and create a polished final product.",
+        "Concept Development: Developed a strong creative concept that would resonate with the target audience.",
+        "Content Creation: Produced high-quality multimedia content that would grab attention and convey the message effectively.",
+        "Media Planning: Implemented a comprehensive media outreach strategy to maximize reach and engagement.",
+        "Message Delivery: Crafted messages that would encourage drivers to adopt safer driving habits.",
       ],
     },
   },
